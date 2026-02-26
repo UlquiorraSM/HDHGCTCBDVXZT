4c75c3264d28e28637cca3d2b9fc3656060531ab
UlquiorraSM <privatservice.ulquiorra@gmail.com>
2026-02-26 02:37:34 +0100

    Add files via upload

---
+++ Backgrounds.js
@@ -0,0 +1,11 @@
+const backgrounds = {
+    "classroom": "https://images.alphacoders.com/694/694833.jpg",
+    "shrine": "https://images.alphacoders.com/131/1318041.png",
+    "village": "https://images.alphacoders.com/132/1322045.jpeg",
+    "forest": "https://images.alphacoders.com/712/712530.jpg",
+    "bedroom": "https://images.alphacoders.com/134/1346210.png"
+};
+
+if (typeof window !== 'undefined') {
+    window.gameBackgrounds = backgrounds;
+}
